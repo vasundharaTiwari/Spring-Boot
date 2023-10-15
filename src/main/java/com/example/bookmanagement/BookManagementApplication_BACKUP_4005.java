1cd81fbf1e2c6349ea8a87aa1a95d1f4d2a08f05
mail.rajeev91@gmail.com <mail.rajeev91@gmail.com>
2023-10-16 00:41:57 +0530

    conflict

---
+++ src/main/java/com/example/bookmanagement/BookManagementApplication_BACKUP_4005.java
@@ -0,0 +1,18 @@
+package com.example.bookmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagementApplication.class, args);
+        System.out.println("hello");
+        System.out.println("git test");
+        System.out.println("First Phase");
+
+
+    }
+
+}
